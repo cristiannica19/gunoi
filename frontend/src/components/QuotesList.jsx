08957cc86dea6acb4335dead85972d975ff166d8
cristiannica19 <cristian.nica19@gmai.com>
2025-04-26 20:55:12 +0300

    ala bun

--- frontend/src/components/QuotesList.jsx
+++ frontend/src/components/QuotesList.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const QuotesList = ({ quotes }) => { 
  return (
  <div className="w-full px-4">
- <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap6 w-full">
+ <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 w-full">
   
   {quotes.map((quote) => ( 
- <li key={quote.id} className="p-4 border rounded-lg shadow bgwhite">
+ <li key={quote.id} className="p-4 border rounded-lg shadow bg-white">
  <p className="text-lg font-bold text-gray-900">{quote.quote}
 </p>
  <span className="text-gray-600">- {quote.author}</span>
